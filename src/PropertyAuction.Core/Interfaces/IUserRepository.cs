2ccbd6435b4eb4fe190f2d4dca962872afd989ff
Lee <nystuenlene@gmail.com>
2026-02-17 23:07:22 +0000

    Added user class model and interface in .Core

---
+++ src/PropertyAuction.Core/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PropertyAuction.Core.Interfaces
+{
+	using PropertyAuction.Core.Models;
+	using System.Threading.Tasks;
+
+	public interface IUserRepository()
+	{
+		Task<User> GetByEmailAsync(string email);
+		Task<User> GetByUsernameAsync(string username);
+		Task AddAsync(User user);
+	}
+}
