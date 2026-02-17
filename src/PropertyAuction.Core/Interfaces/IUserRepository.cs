de8783fc44620201aeeba6cd6079354227b7b70c
Gaia <gaiafiorillo06@gmail.com>
2026-02-17 23:55:43 +0000

    fix interface syntax

--- src/PropertyAuction.Core/Interfaces/IUserRepository.cs
+++ src/PropertyAuction.Core/Interfaces/IUserRepository.cs
@@ -5,7 +5,7 @@ namespace PropertyAuction.Core.Interfaces
 	using PropertyAuction.Core.Models;
 	using System.Threading.Tasks;
 
-	public interface IUserRepository()
+	public interface IUserRepository
 	{
 		Task<User> GetByEmailAsync(string email);
 		Task<User> GetByUsernameAsync(string username);
