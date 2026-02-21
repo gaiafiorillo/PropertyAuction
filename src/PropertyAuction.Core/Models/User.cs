9e966e1aeeb90ea9142965b92c7fdff3335e0c31
Lee <nystuenlene@gmail.com>
2026-02-21 19:27:34 +0000

    added required fields to username, email and passwordhash.

--- src/PropertyAuction.Core/Models/User.cs
+++ src/PropertyAuction.Core/Models/User.cs
@@ -6,9 +6,9 @@ namespace PropertyAuction.Core.Models
 	public class User
 	{
 		public int Id { get; set; }
-		public string Username { get; set; }
-		public string Email { get; set; }
-		public string PasswordHash { get; set; }
+		public required string Username { get; set; }
+        public required string Email { get; set; }
+		public required string PasswordHash { get; set; }
 
 		// When the account was created
 		public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
